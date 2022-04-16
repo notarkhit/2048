85d97e8bb9be7fd1dc6ea0ac470ba0a9cf632e7f
Aryan Arkhit Behra <93533363+notarkhit@users.noreply.github.com>
2022-04-16 16:48:40 +0530

    Generate random tiles
    - made getters for new random tiles
    
    - generate random x & y coordinates for positioning the newly generated tiles
    
    - made getters for empty cells ( cells that do not contain a tile )

--- Grid.js
+++ Grid.js
@@ -14,17 +14,31 @@ export default class Grid {
     })
     console.log(this.cells)
   }
+
+  get #emptyCells() {
+    return this.#cells.filter(cell => cell.tile == null)
+  }
+
+  randomEmptyCell() {
+    const randomIndex = Math.floor(Math.random() * this.#emptyCells.length)
+    return this.#emptyCells[randomIndex]
+  }
 }
 
 class Cell {
   #cellElement
   #x
   #y
+  #tile
 
    constructor(cellElement , x , y ) {
      this.#cellElement = cellElement
      this.#x = x
      this.#y = y 
+   }
+
+   get tile() {
+     return this.#tile
    }
 }
 
