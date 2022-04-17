8a728badd790805910acc753fa244ec9ad0c70db
Aryan Arkhit Behra <93533363+notarkhit@users.noreply.github.com>
2022-04-17 12:22:06 +0530

    Coded the logic of the game
    - made getters for x , y and merge tile of the Grid class
    
    - Added an event listener for arrow keys
    
    - got the cells by rows and columns (for horizontal and vertical movements)
    
    - used slide Tiles helper function for the tile movement
    
    > Todo: write code for tiles merging and generating new tiles after every move

--- script.js
+++ script.js
@@ -6,3 +6,76 @@ const gameBoard = document.getElementById("game-board")
 const grid = new Grid(gameBoard)
 grid.randomEmptyCell().tile = new Tile(gameBoard)
 grid.randomEmptyCell().tile = new Tile(gameBoard)
+setupInput()
+
+function setupInput() {
+    window.addEventListener("keydown",handleInput, { once: true })
+}
+
+function handleInput (e) {
+
+  switch(e.key){
+    case "ArrowUp" :
+      moveUp()
+      break;
+
+    case "ArrowDown" :
+      moveDown()
+      break;
+
+    case "ArrowLeft" :
+      moveLeft()
+      break;
+
+    case "ArrowRight" :
+      moveRight()
+      break;
+    
+    default:
+      setupInput()
+      return
+  }
+
+// Other code
+  
+  setupInput()
+}
+
+function moveUp() {
+  slideTiles(grid.cellsByColumn)
+}
+
+function moveDown() {
+  slideTiles(grid.cellsByColumn.map(column => [...column].reverse()))
+}
+
+function moveLeft() {
+  slideTiles(grid.cellsByRow)
+}
+
+function moveRight() {
+  slideTiles(grid.cellsByRow.map(row => [...row].reverse()))
+}
+
+function slideTiles(cells) {
+  cells.forEach(group => {
+    for(let i = 1 ; i < group.length ; i++ ) {
+      const cell = group[i]
+      if ( cell.tile == null) continue
+      let lastValidCell
+      for (let j = i - 1 ; j >= 0 ; j-- ) {
+        const moveToCell = group[j]
+        if(!moveToCell.canAccept(cell.tile)) break 
+        lastValidCell = moveToCell
+      }
+      if(lastValidCell != null) {
+        if(lastValidCell.tile != null){
+          lastValidCell.mergeTile = cell.tile
+        } else {
+          lastValidCell.tile = cell.tile
+        }
+        cell.tile = null
+      }
+    }
+  })
+}
